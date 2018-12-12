c7f45c75deaca693dc572aa233d645538814d312
gyh <111111>
2018-12-12 09:02:38 +0800

    first commit 一个简单的事件广播与订阅

---
+++ eventbus.js
@@ -0,0 +1,61 @@
+class Eventbus {
+    constructor() {
+    }
+
+    /**
+     * 注册事件
+     * @param {String} name 事件名称 
+     * @param {*} context page/component实例
+     * @param {*} callback 事件回调
+     */
+    on(name, context, callback) {
+        const id = Eventbus.events.length;
+        const group = {
+            id: id,
+            ctx: context,
+            cb: callback
+        };
+        if (Array.isArray(Eventbus.events[name])) {
+            Eventbus.events[name].push(group)
+        } else {
+            Eventbus.events[name] = [group];
+        }
+    }
+    /**
+     * 触发事件
+     * @param {String} name 事件名称 
+     * @param {*} res 回调携带数据
+     */
+    emit(name, res) {
+        const groups = Eventbus.events[name];
+        if (Array.isArray(groups)) {
+            groups.forEach(group => {
+                const context = group.ctx;
+                const callback = group.cb;
+                const eventId = group.id;
+                callback.call(context, {
+                    data: res,
+                    event_id: eventId
+                });
+            })
+        }
+    }
+    /**
+     * 销毁某事件, 触发完事件后记得销毁
+     * @param {String} name 事件名称 
+     * @param {String} id 时间id, 由emit返回参数携带
+     */
+    destroy(name, id) {
+        const groups = Eventbus.events[name];
+        if (Array.isArray(groups)) {
+            Eventbus.events[name] = groups.filter(group => {
+                return group.id != id
+            })
+        }
+        if(Eventbus.events[name].length === 0){
+            delete Eventbus.events[name]
+        }
+    }
+}
+Eventbus.events = []; // 模拟静态属性
+module.exports = Eventbus;
